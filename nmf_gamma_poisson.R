88b7ee43c2d07eca6c5a6a4a795a41c8429611ea
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-08 22:26:50 -0800

    Fit stan model to poisson gamma

--- nmf_gamma_poisson.R
+++ nmf_gamma_poisson.R
@@ -1,7 +1,7 @@
 #! /usr/bin/env Rscript
 
 ## File description -------------------------------------------------------------
-## Experiment running gamma-poisson factorization model, on simulated data.
+## Experiment running gamma-pmesoisson factorization model, on simulated data.
 ##
 ## reference:https://arxiv.org/pdf/1506.03431.pdf
 
@@ -11,6 +11,7 @@ library("dplyr")
 library("reshape2")
 library("ggplot2")
 library("rstan")
+options(mc.cores = parallel::detectCores())
 
 ## ---- simulate ----
 stan_data <- list(
@@ -41,3 +42,19 @@ y <- matrix(
   rpois(N * P, theta %*% t(beta)),
   N, P
 )
+
+## ---- stan-fit ----
+f <- stan_model("nmf_gamma_poisson.stan")
+fit <- extract(
+  vb(f, data = stan_data)
+)
+
+## ---- examine ----
+theta_fit <- melt(
+  fit$theta,
+  varnames = c("iteration", "i", "k")
+)
+
+ggplot(theta_fit) +
+  geom_histogram(aes(x = iterations)) +
+  facet_wrap(k ~ i)
