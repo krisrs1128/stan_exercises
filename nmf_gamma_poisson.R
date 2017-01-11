3e79a21f090dff5216c13dcf5c37b388841f7d2f
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-10 19:37:27 -0800

    Remove rednudant plot, and use gibbs sampling instead

--- nmf_gamma_poisson.R
+++ nmf_gamma_poisson.R
@@ -96,9 +96,9 @@ ggplot(data.frame(
   geom_abline(slope = 1)
 
 ## ---- stan-fit ----
-f <- stan_model("nmf_gamma_poisson.stan")
+#f <- stan_model("nmf_gamma_poisson.stan")
 fit <- extract(
-  vb(f, data = stan_data)
+  stan(file = "nmf_gamma_poisson.stan", data = stan_data, chains = 1)
 )
 
 ## ---- examine ----
@@ -127,30 +127,13 @@ theta_fit$i <- factor(
   levels = theta_levels
 )
 
-ggplot(
-  theta_fit %>%
-  filter(as.numeric(i) <= 25)
-) +
-  geom_histogram(
-    aes(x = value, fill = as.factor(k)),
-    bins = 100, alpha = 0.6) +
-  coord_flip() +
-  facet_grid(. ~ i) +
-  xlim(0, 6) +
-  theme(
-    panel.spacing = unit(0, "line")
-  ) +
-  geom_vline(
-    aes(xintercept = truth, col = as.factor(k))
-  )
-
 theta_fit_cast <- theta_fit %>%
   data.table::setDT() %>%
   data.table::dcast(i + iteration ~ k, value.var = c("value", "truth"))
 
 ggplot() +
   geom_text(
-    data = theta_fit_cast %>% filter(iteration > 100),
+    data = theta_fit_cast,
     aes(x = value_1, y = value_2, label = i),
     size = 2, alpha = 0.1, col = "#5E5E5E"
   ) +
@@ -166,7 +149,7 @@ ggplot() +
 
 ggplot() +
   geom_text(
-    data = theta_fit_cast %>% filter(iteration > 100),
+    data = theta_fit_cast,
     aes(x = value_1, y = value_2, label = i),
     size = 2, alpha = 0.1, col = "#5E5E5E"
   ) +
